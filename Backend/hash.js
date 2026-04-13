500ba960ab50e909c30903e89a9c078b340bf868
SagarKhanna <sagarkhanna.dev@gmail.com>
2026-04-13 11:13:57 +0530

    implemented login authentication and routing for admin and client page

--- Backend/hash.js
+++ Backend/hash.js
@@ -2,7 +2,13 @@ import bcrypt from "bcryptjs";
 
 const run = async () => {
     const hash = await bcrypt.hash("229801", 10);
+    const adminHash = await bcrypt.hash("admin123", 10);
+    const clientHash = await bcrypt.hash("client123", 10);
+
+
     console.log(hash);
+    console.log(adminHash);
+    console.log(clientHash);
 };
 
 run();
