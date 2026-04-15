74381c7d4fb6f2270fe6e8dfea3967df3740d27a
SagarKhanna <sagarkhanna.dev@gmail.com>
2026-04-15 14:02:02 +0530

    restored full backup code

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import { ClientProvider } from "./context/ClientContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ClientProvider>
-    <App />
-  </ClientProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
