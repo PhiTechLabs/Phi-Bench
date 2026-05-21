c14e23dbca0a7fcde8af54465d2b20dfa4866fb4
SagarKhanna <sagarkhanna.dev@gmail.com>
2026-05-21 17:26:35 +0530

    RBAC Backend implemented in order.

--- Backend/seeders/defaultRoles.js
+++ Backend/seeders/defaultRoles.js
@@ -1,4 +1,151 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import connectDB from "../config/db.js";
+// import Role from "../models/Role.js";
+// import { PERMISSIONS } from "../config/permissions.js";
+
+// dotenv.config();
+
+// const seedRoles = async () => {
+//     try {
+//         await connectDB();
+
+//         for (const roleData of roles) {
+
+//             await Role.findOneAndUpdate(
+
+//                 { name: roleData.name.toLowerCase() },
+
+//                 {
+//                     ...roleData,
+//                     name: roleData.name.toLowerCase(),
+//                 },
+
+//                 {
+//                     upsert: true,
+//                     new: true,
+//                 }
+
+//             );
+
+//         }
+
+//         const roles = [
+//     {
+//         name: "superAdmin",
+//         hierarchyLevel: 1,
+//         dataScope: "ORGANIZATION",
+//         isSystemRole: true,
+//         permissions: ["*"],
+//     },
+
+//     {
+//         name: "admin",
+//         hierarchyLevel: 2,
+//         dataScope: "BRANCH",
+//         isSystemRole: true,
+
+//         permissions: [
+
+//             // Candidates
+//             PERMISSIONS.CANDIDATE_VIEW,
+//             PERMISSIONS.CANDIDATE_CREATE,
+//             PERMISSIONS.CANDIDATE_EDIT,
+//             PERMISSIONS.CANDIDATE_DELETE,
+
+//             // Jobs
+//             PERMISSIONS.JOB_VIEW,
+//             PERMISSIONS.JOB_CREATE,
+//             PERMISSIONS.JOB_EDIT,
+//             PERMISSIONS.JOB_DELETE,
+
+//             // Clients
+//             PERMISSIONS.CLIENT_VIEW,
+//             PERMISSIONS.CLIENT_CREATE,
+//             PERMISSIONS.CLIENT_EDIT,
+//             PERMISSIONS.CLIENT_DELETE,
+
+//             // Users
+//             PERMISSIONS.USER_VIEW,
+//             PERMISSIONS.USER_CREATE,
+//             PERMISSIONS.USER_EDIT,
+//             PERMISSIONS.USER_DELETE,
+
+//             // Roles
+//             PERMISSIONS.ROLE_VIEW,
+//             PERMISSIONS.ROLE_CREATE,
+//             PERMISSIONS.ROLE_EDIT,
+//             PERMISSIONS.ROLE_DELETE,
+//         ],
+//     },
+
+//     {
+//         name: "teamLead",
+//         hierarchyLevel: 3,
+//         dataScope: "TEAM",
+//         isSystemRole: true,
+
+//         permissions: [
+
+//             // Candidates
+//             PERMISSIONS.CANDIDATE_VIEW,
+//             PERMISSIONS.CANDIDATE_CREATE,
+//             PERMISSIONS.CANDIDATE_EDIT,
+
+//             // Jobs
+//             PERMISSIONS.JOB_VIEW,
+//             PERMISSIONS.JOB_CREATE,
+
+//             // Clients
+//             PERMISSIONS.CLIENT_VIEW,
+//         ],
+//     },
+
+//     {
+//         name: "recruiter",
+//         hierarchyLevel: 4,
+//         dataScope: "SELF",
+//         isSystemRole: true,
+
+//         permissions: [
+
+//             // Candidates
+//             PERMISSIONS.CANDIDATE_VIEW,
+//             PERMISSIONS.CANDIDATE_CREATE,
+
+//             // Jobs
+//             PERMISSIONS.JOB_VIEW,
+
+//             // Optional
+//             PERMISSIONS.CLIENT_VIEW,
+//         ],
+//     },
+
+//     {
+//         name: "client",
+//         hierarchyLevel: 5,
+//         dataScope: "CUSTOM",
+//         isSystemRole: true,
+
+//         permissions: [
+//             PERMISSIONS.JOB_VIEW,
+//         ],
+//     },
+// ];
+
+
+//         console.log("Default roles seeded");
+//         process.exit();
+
+//     } catch (error) {
+//         console.error(error);
+//         process.exit(1);
+//     }
+// };
+
+// seedRoles();
+
+
 import dotenv from "dotenv";
 import connectDB from "../config/db.js";
 import Role from "../models/Role.js";
@@ -6,15 +153,10 @@ import { PERMISSIONS } from "../config/permissions.js";
 
 dotenv.config();
 
-const seedRoles = async () => {
-    try {
-        await connectDB();
+const roles = [
 
-        await Role.deleteMany();
-
-        const roles = [
     {
-        name: "superAdmin",
+        name: "super_admin",
         hierarchyLevel: 1,
         dataScope: "ORGANIZATION",
         isSystemRole: true,
@@ -62,23 +204,20 @@ const seedRoles = async () => {
     },
 
     {
-        name: "teamLead",
+        name: "team_lead",
         hierarchyLevel: 3,
         dataScope: "TEAM",
         isSystemRole: true,
 
         permissions: [
 
-            // Candidates
             PERMISSIONS.CANDIDATE_VIEW,
             PERMISSIONS.CANDIDATE_CREATE,
             PERMISSIONS.CANDIDATE_EDIT,
 
-            // Jobs
             PERMISSIONS.JOB_VIEW,
             PERMISSIONS.JOB_CREATE,
 
-            // Clients
             PERMISSIONS.CLIENT_VIEW,
         ],
     },
@@ -91,14 +230,11 @@ const seedRoles = async () => {
 
         permissions: [
 
-            // Candidates
             PERMISSIONS.CANDIDATE_VIEW,
             PERMISSIONS.CANDIDATE_CREATE,
 
-            // Jobs
             PERMISSIONS.JOB_VIEW,
 
-            // Optional
             PERMISSIONS.CLIENT_VIEW,
         ],
     },
@@ -113,17 +249,49 @@ const seedRoles = async () => {
             PERMISSIONS.JOB_VIEW,
         ],
     },
+
 ];
 
-        await Role.insertMany(roles);
+const seedRoles = async () => {
 
-        console.log("Default roles seeded");
+    try {
+
+        await connectDB();
+
+        for (const roleData of roles) {
+
+            await Role.findOneAndUpdate(
+
+                {
+                    name: roleData.name.toLowerCase(),
+                },
+
+                {
+                    ...roleData,
+                    name: roleData.name.toLowerCase(),
+                },
+
+                {
+                    upsert: true,
+                    new: true,
+                }
+
+            );
+
+        }
+
+        console.log("Default roles seeded successfully");
+
         process.exit();
 
     } catch (error) {
+
         console.error(error);
+
         process.exit(1);
+
     }
+
 };
 
 seedRoles();
