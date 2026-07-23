98a78b95cd775ad9326b2f430e4b3ad9e72fbc52
Sagar Khanna <sagarkhanna.dev@gmail.com>
2026-07-23 11:23:45 +0530

    Home page component Completed

--- Frontend/src/Landing_Pages/Landing_Page.jsx
+++ Frontend/src/Landing_Pages/Landing_Page.jsx
@@ -1,10 +1,12 @@
 import Landing_Navbar from "./Landing_Navbar";
+import LandingHome from "./Landing_Home";
 
 function Landing_Page() {
     return (
         <div className="min-h-screen">
 
             <Landing_Navbar />
+            <LandingHome />
 
             {/* Landing Page Content Will Come Here */}
 
